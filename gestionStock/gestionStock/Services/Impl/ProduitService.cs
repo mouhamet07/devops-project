c7328344efde2ef4e883be0074446bdfb32d8c0d
Maryse <maryseokana@gmail.com>
2026-03-28 21:10:59 +0000

    service/ajout produit

--- gestionStock/gestionStock/Services/Impl/ProduitService.cs
+++ gestionStock/gestionStock/Services/Impl/ProduitService.cs
@@ -1,7 +1,40 @@
+using gestionStock.Data;
+using gestionStock.Models;
+
 namespace gestionStock.Services.Impl
 {
-    public interface ProduitService
+    public class ProduitService : IProduitService 
     {
+        private readonly AppDbContext _context;
+        public ProduitService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddProduit(Produit produit)
+        {
+            if (produit == null)
+                throw new ArgumentNullException(nameof(produit));
+            if (produit.Quantite <= 0)
+            {
+                produit.etat = EtatProduit.EN_RUPTURE;
+                produit.Quantite = 0; 
+            }
+            else
+            {
+                produit.etat = EtatProduit.EN_STOCK;
+            }
+
+            produit.IsArchived = false;
         
+            Categorie categorie = _context.Categories.Find(produit.categorie.Id);
+            if (categorie == null)
+                throw new Exception("Cette catégorie n'existe pas");
+
+            produit.categorie = categorie;
+
+            _context.Produits.Add(produit);
+            _context.SaveChanges();
+        }
     }
-}
+    }
